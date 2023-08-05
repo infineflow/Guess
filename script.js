ecfe06da3d30c6deec3ba469894c27240118f87a
Sakshi Raghuwanshi <139263994+infineflow@users.noreply.github.com>
2023-08-05 23:52:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const btnEl = document.getElementById("btn")
+const bmiInputEl = document.getElementById("bmi-result")
+const weightConditionEl = document.getElementById("weight-condition")
+
+function calculateBMI(){
+    const heightValue = document.getElementById
+    ("height").value / 100
+    const weightValue = document.getElementById
+    ("weight").value
+
+
+    const bmiValue = weightValue / (heightValue*heightValue)
+
+    bmiInputEl.value = bmiValue;
+
+    if(bmiValue < 18.5){
+        weightConditionEl.innerText = "Under weight😌";
+
+    }else if(bmiValue >= 18.5 && bmiValue <= 24.9)
+    {
+        weightConditionEl.innerText = "Normal weight😎";
+    }else if(bmiValue >= 25 && bmiValue <= 29.9){
+        weightConditionEl.innerText = "Over weight😯";
+    }
+    else if(bmiValue >=30){
+        weightConditionEl.innerText = "Obesity😥";
+    }
+
+   
+}
+
+
+
+
+
+
+
+
+btnEl.addEventListener("click", calculateBMI)
